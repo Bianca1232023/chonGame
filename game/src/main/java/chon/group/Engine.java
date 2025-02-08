e7e3d9b7dcb51ea1f2bdbcf178653d000dcb2ee9
Bianca1232023 <150062646+Bianca1232023@users.noreply.github.com>
2025-02-07 23:41:52 -0300

    "CODIGO QUE CONTEM A NAVE"

--- game/src/main/java/chon/group/Engine.java
+++ game/src/main/java/chon/group/Engine.java
@@ -1,158 +1,162 @@
 package chon.group;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-import chon.group.game.domain.agent.Agent;
-import chon.group.game.domain.environment.Environment;
-import chon.group.game.drawer.EnvironmentDrawer;
-import chon.group.game.drawer.JavaFxMediator;
+import chon.group.agent.Agent;
+import chon.group.agent.HeroMovement;
+import chon.group.enviroment.Environment;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-/**
- * The {@code Engine} class represents the main entry point of the application
- * and serves as the game engine for "Chon: The Learning Game."
- * <p>
- * This class extends {@link javafx.application.Application} and manages the
- * game initialization, rendering, and main game loop using
- * {@link javafx.animation.AnimationTimer}.
- * </p>
- * 
- * <h2>Responsibilities</h2>
- * <ul>
- * <li>Set up the game environment, agents, and graphical components.</li>
- * <li>Handle keyboard input for controlling the protagonist agent.</li>
- * <li>Execute the game loop for updating and rendering the game state.</li>
- * </ul>
- */
 public class Engine extends Application {
 
-    /* If the game is paused or not. */
-    private boolean isPaused = false;
+	private final ArrayList<String> input = new ArrayList<>();
 
-    /**
-     * Main entry point of the application.
-     *
-     * @param args command-line arguments passed to the application.
-     */
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage theStage) {
+		try {
 
-    /**
-     * Starts the JavaFX application and initializes the game environment, agents,
-     * and graphical components.
-     * <p>
-     * This method sets up the game scene, handles input events, and starts the
-     * game loop using {@link AnimationTimer}.
-     * </p>
-     *
-     * @param theStage the primary stage for the application.
-     */
-    @Override
-    public void start(Stage theStage) {
-        try {
-            /* Initialize the game environment and agents */ 
-            Environment environment = new Environment(0, 0, 1280, 780, "/images/environment/castle.png");
-            Agent chonBota = new Agent(400, 390, 90, 65, 3, 1000, "/images/agents/chonBota.png", false);
-            Agent chonBot = new Agent(920, 440, 90, 65, 1, 3, "/images/agents/chonBot.png", true);
-            environment.setProtagonist(chonBota);
-            environment.getAgents().add(chonBot);
-            environment.setPauseImage("/images/environment/pause.png");
+			ArrayList<Agent> agents = new ArrayList<>();
 
-            /* Set up the graphical canvas */ 
-            Canvas canvas = new Canvas(environment.getWidth(), environment.getHeight());
-            GraphicsContext gc = canvas.getGraphicsContext2D();
-            EnvironmentDrawer mediator = new JavaFxMediator(environment, gc);
+			Agent agentA = new Agent(920, 420, 134, 80, "/images/agent/Asteroid/asteroide1.png", false, 0);
+			agents.add(agentA);
 
-            /* Set up the scene and stage */ 
-            StackPane root = new StackPane();
-            Scene scene = new Scene(root, environment.getWidth(), environment.getHeight());
-            theStage.setTitle("Chon: The Learning Game");
-            theStage.setScene(scene);
+			Agent agentB = new HeroMovement(70, 410, 120, 100, "/images/agent/Spaceship/spaceship_andando1.png", true, 500000);
+			agents.add(agentB);
 
-            root.getChildren().add(canvas);
-            theStage.show();
+			if (Agent.numberProtagonist == 1) {
 
-            /* Handle keyboard input */
-            ArrayList<String> input = new ArrayList<String>();
-            scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-                public void handle(KeyEvent e) {
-                    String code = e.getCode().toString();
-                    input.clear();
+				StackPane root = new StackPane();
+				Scene scene = new Scene(root, 960, 870);
+				theStage.setTitle("Chon: The Learning Game");
+				theStage.setScene(scene);
+				Canvas canvas = new Canvas(960, 870);
 
-                    System.out.println("Pressed: " + code);
+				System.out.println("Working Directory = " + System.getProperty("user.dir"));
 
-                    if (code.equals("P")) {
-                        isPaused = !isPaused;
-                    }
+				root.getChildren().add(canvas);
+				theStage.show();
 
-                    if (!isPaused && !input.contains(code)) {
-                        input.add(code);
-                    }
+				Environment atmosphere = new Environment(0, 0, 960, 870, "/images/environment/background space.png",
+						agents, canvas.getGraphicsContext2D());
 
-                }
-            });
+				Environment lifeIcon = new Environment(964, 638, 56, 47, "/images/agent/Spaceship/Life_icon.png",
+						agents, canvas.getGraphicsContext2D());
 
-            scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-                public void handle(KeyEvent e) {
-                    String code = e.getCode().toString();
-                    System.out.println("Released: " + code);
-                    input.remove(code);
-                }
-            });
+				startAnimation(canvas, scene, atmosphere, lifeIcon, agents);
 
-            /* Start the game loop */ 
-            new AnimationTimer() {
+				theStage.show();
+			} else {
+				throw new IllegalArgumentException(
+						"The number of protagonists didn't reach the minimum number or exceeded it");
+			}
 
-                /**
-                 * The game loop, called on each frame.
-                 *
-                 * @param now the timestamp of the current frame in nanoseconds.
-                 */
-                @Override
-                public void handle(long arg0) {
-                    mediator.clearEnvironment();
-                    /* Branching the Game Loop */
-                    if (isPaused) {
-                        mediator.drawBackground();
-                        mediator.drawAgents();
-                        /* Rendering the Pause Screen */
-                        mediator.drawPauseScreen();
-                    } else {
-                        /* ChonBota Only Moves if the Player Press Something */
-                        /* Update the protagonist's movements if input exists */
-                        if (!input.isEmpty()) {
-                            /* ChonBota's Movements */
-                            environment.getProtagonist().move(input);
-                            environment.checkBorders();
-                        }
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			Platform.exit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			Platform.exit();
+		}
+	}
 
-                        /* ChonBot's Automatic Movements */
-                        /* Update the other agents' movements */ 
-                        environment.getAgents().get(0).chase(environment.getProtagonist().getPosX(),
-                                environment.getProtagonist().getPosY());
+	public void startAnimation(Canvas canvas, Scene scene, Environment atmosphere, Environment lifeIcon,
+			ArrayList<Agent> agents) {
 
-                        /* Render the game environment and agents */ 
-                        environment.detectCollision();
-                        mediator.drawBackground();
-                        mediator.drawAgents();
-                    }
-                }
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
+				String code = e.getCode().toString();
+				input.clear();
 
-            }.start();
-            theStage.show();
+				System.out.println("Pressed: " + code);
+				input.add(code);
+			}
+		});
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
+			public void handle(KeyEvent e) {
+				String code = e.getCode().toString();
+				System.out.println("Released: " + code);
+				input.remove(code);
+			}
+		});
+
+		new AnimationTimer() {
+
+			@Override
+			public void handle(long arg0) {
+				atmosphere.setGc(canvas.getGraphicsContext2D());
+
+				if (agents.size() > 1)
+					atmosphere.getAgents().get(0).move("LEFT");
+
+				if (!input.isEmpty()) {
+					atmosphere.getProtagonist().move(input.get(0));
+					if (atmosphere.limitsApprove()) {
+						atmosphere.drawAgents(agents);
+					}
+				}
+				atmosphere.clearRect();
+				atmosphere.drawBackground();
+
+				if (atmosphere.limitsApprove()) {
+					atmosphere.drawAgents(agents);
+					for (int i = 0; i < 3; i++) {
+						lifeIcon.setPositionX(964 + (i * (lifeIcon.getWidth() + 10)));
+						lifeIcon.drawLifeIcon();
+					}
+				}
+
+				if (agents.size() > 1) {
+					Agent asteroid = atmosphere.getAgents().get(0); // O asteroide (primeiro agente da lista)
+					Agent spaceship = atmosphere.getAgents().get(1); // A espaçonave (segundo agente da lista)
+					// Verifica a colisão e inicia a animação de explosão
+					if (atmosphere.checkCollision(asteroid, spaceship)) {
+						System.out.println("Colisão detectada entre o asteroide e a espaçonave!");
+						asteroid.setAlive(false);
+						asteroid.startExplosion(); // Inicia a explosão do asteroide
+
+						spaceship.desacrease_life(1); // Reduz a vida em 1
+						System.out.println("A nave tem: " + spaceship.getLife() + " de vida");
+
+						if (spaceship.getLife() <= 0) {
+							spaceship.startExplosion(); // Inicia a explosão da nave ao chegar em 0 de vida
+							System.out.println("A nave foi destruída!");
+						}
+
+						// Atualiza a animação da explosão
+						asteroid.updateExplosion(); // Agora a explosão será controlada pelo tempo no método
+						// updateExplosion()
+					}
+
+				} else {
+					Agent spaceship = atmosphere.getAgents().get(0);
+				}
+
+				// Redesenha os agentes
+				for (Iterator<Agent> iterator = agents.iterator(); iterator.hasNext();) {
+					Agent agent = iterator.next();
+					if (!agent.isAlive()) {
+						iterator.remove();
+					}
+
+				}
+
+				atmosphere.drawAgents(agents);
+			}
+
+		}.start();
+	}
 }
